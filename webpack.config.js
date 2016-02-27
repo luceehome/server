f0b59ef134278089548d8fd2afd3dcd85b8bf491
Alex Dik <mail@alexdik.de>
2016-02-27 12:07:21 +0100

    Add webpack web client configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,37 @@
+const commonLoaders = [
+  {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+];
+
 module.exports = [
   {
     name: 'server',
     entry: './src/server/server.js',
     target: 'node',
     output: {
-      path: __dirname + '/build/server',
-      filename: 'index.js'
+      path: __dirname + '/build',
+      filename: 'server.js'
     },
     module: {
       loaders: [
-        {test: /\.json$/, loader: 'json-loader'},
-        {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
-      ]
+        {test: /\.json$/, loader: 'json-loader'}
+      ].concat(commonLoaders)
     },
     externals: /^[a-z\-0-9]+$/,
     node: {
       __dirname: true,
       __filename: true
+    }
+  },
+  {
+    name: 'web-client',
+    entry: './src/web-client/client.js',
+    output: {
+      path: __dirname + '/src/server/public/javascripts',
+      filename: 'bundle.js'
+    },
+    module: {
+      loaders: [
+      ].concat(commonLoaders)
     }
   }
 ];
